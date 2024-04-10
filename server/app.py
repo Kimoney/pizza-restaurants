32e821818c4d4f8e40f4f50e0f8d31930f0a07d0
John <johntaykimani@gmail.com>
2024-04-10 16:02:31 +0300

    feat: Connected models to app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-# from flask_migrate import Migrate
-
+from flask_migrate import Migrate
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 @app.route('/')
 def index():
