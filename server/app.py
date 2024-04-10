c7f34c227c3db98d5dd70802740b579d4d6d1650
John <johntaykimani@gmail.com>
2024-04-10 15:37:12 +0300

    feat: Add required routes endpoints.

---
+++ server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+# from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Pizza Restaurants</h1>'
+
+@app.route('/restaurants')
+def restaurants():
+    return '<h1>Return JSON data of the Restaurant Model</h1>'
+
+@app.route('/restaurants/<int:id>', methods=['GET', 'DELETE'])
+def restaurants_by_id(id):
+    return '<h1>If the Restaurant exists, return JSON with associated pizzas </h1>'
+
+@app.route('/pizzas')
+def pizzas():
+    return '<h1>Return JSON data of the Pizza Model</h1>'
+
+@app.route('/restaurant_pizzas')
+def restaurant_pizzas():
+    return '<h1>This route should create a new RestaurantPizza that is associated with an existing Pizza and Restaurant.</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
